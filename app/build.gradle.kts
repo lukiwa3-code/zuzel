6dd3ffc844e06816735b5964f2b127119f246bca
lukiwa3-code <lukiwa3@gmail.com>
2026-04-25 23:46:31 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.multiplicationtable"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildFeatures {
